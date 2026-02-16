9ff0383738026ff98b16cacf64e277e4b548180b
Sergey Tretyak <sergey-grey2009@yandex.ru>
2026-02-16 23:12:23 +0300

    favicon dockerfile

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,7 +29,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ message: 'Путь не найден', path: req.originalUrl });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const MONGO_URI =
   process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/time_management';
 
